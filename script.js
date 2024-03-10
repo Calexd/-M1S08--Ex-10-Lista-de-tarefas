fee1f552f445a87ef14b36a116427a8ef622ffb2
[calexd] <[caledarvs@gmail.com]>
2024-03-10 19:43:03 -0300

    Finished task basic functions javascript code

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
-let boxNewTask = document.getElementById('form-task-new')
-let boxCompletedTask = document.getElementById('form-task-completed')
-let textNewTask = document.getElementById('new-task')
 let buttonNewTask = document.getElementById('button-task')
+let buttonRemoveTask = document.getElementById('button-remove-task')
 
 buttonNewTask.addEventListener('click', newTask)
+buttonNewTask.addEventListener('click', () => {
+    let textNewTask = document.getElementById('new-task')
+    textNewTask.value = ''
+})
+
+buttonRemoveTask.addEventListener('click', () => {
+    let boxCompletedTask = document.getElementById('form-task-completed')
+    let elements = boxCompletedTask.querySelectorAll('input[type="checkbox"]:checked')
+    elements.forEach(elements =>{
+        elements.parentElement.remove()
+    })
+})
 
 function newTask(){
+    let boxNewTask = document.getElementById('form-task-new')
+    let textNewTask = document.getElementById('new-task').value
     let newTask = document.createElement('div')
-        newTask.className = 'oneTask'
+    newTask.className = 'oneTask'
+    
     let newTaskCheck = document.createElement('input')
-        newTaskCheck.type = 'checkbox'
-        newTaskCheck.className = 'checkOneTask'
-        // newTaskCheck.addEventListener('change', moveTask)
-
+    newTaskCheck.type = 'checkbox'
+    newTaskCheck.className = 'checkOneTask'
+    newTaskCheck.addEventListener('change', moveTask)
+    
     let newLabelTask = document.createElement('label')
-        newLabelTask.innerText = 'textNewTask'
-
+    newLabelTask.innerText= textNewTask
+    
     boxNewTask.appendChild(newTask)
-    newTask.appendChild(newLabelTask)
     newTask.appendChild(newTaskCheck)
+    newTask.appendChild(newLabelTask)
+    
 }
+
+function moveTask(){
+    let boxCompletedTask = document.getElementById('form-task-completed')
+    let boxNewTask = document.getElementById('form-task-new')
+    let task = this.parentElement
+
+    if (this.checked){
+        boxCompletedTask.appendChild(task)
+    } else {
+        boxNewTask.appendChild(task)
+    }
+}
+
+
+
+
+
+
+
+
+
